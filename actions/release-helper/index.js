1172636e27034075df2796214f5055fc934e8293
iancha1992 <heec@google.com>
2023-05-31 16:17:46 -0700

    payload

--- actions/release-helper/index.js
+++ actions/release-helper/index.js
@@ -1,1 +1,11 @@
-console.log("Javascript hello world!!!!!")
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+console.log("Javascript hello world!!!!!");
+
+const payload = github.context.payload;
+
+console.log("This is the payload")
+console.log(payload)
+
+
