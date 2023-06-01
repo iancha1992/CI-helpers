d3800b900968011c82f802a0fd219af348c86b33
iancha1992 <heec@google.com>
2023-06-01 16:17:42 -0700

    Testing

--- actions/release-helper/index.js
+++ actions/release-helper/index.js
@@ -11,7 +11,7 @@ const octokit = github.getOctokit(token);
 //     auth: 'YOUR-TOKEN'
 // })
   
-const result = octokit.request('GET /repos/iancha1992/gh_practice/issues/9/comments', {
+const result = await octokit.request('GET /repos/iancha1992/gh_practice/issues/9/comments', {
     owner: 'OWNER',
     repo: 'REPO',
     issue_number: '9',
