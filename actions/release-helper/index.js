412ab8d617221a7ee9ddbb447587ec4d19553017
iancha1992 <heec@google.com>
2023-06-01 15:56:59 -0700

    Testing

--- actions/release-helper/index.js
+++ actions/release-helper/index.js
@@ -1,9 +1,32 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+
+const token = core.getInput("token");
+const octokit = github.getOctokit(token);
+
+// Octokit.js
+// https://github.com/octokit/core.js#readme
+// const octokit = new Octokit({
+//     auth: 'YOUR-TOKEN'
+// })
+  
+// await octokit.request('GET /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+//     owner: 'OWNER',
+//     repo: 'REPO',
+//     issue_number: '8',
+//     headers: {
+//         'X-GitHub-Api-Version': '2022-11-28'
+//     }
+// })
+
+
+
+
 console.log("Javascript hello world!!!!!");
 
 const payload = github.context.payload;
+
 
 console.log("This is the payload")
 console.log(payload);
@@ -11,12 +34,12 @@ console.log(payload);
 
 console.log("This is the issue!!!!!")
 console.log(payload.issue.number)
+issue_number = payload.issue.number
 
 
 
 
-
-
+issue_number = `https://api.github.com/repos/iancha1992/gh_practice/issues/${issue_number}/comments`
 
 
 // https://api.github.com/repos/iancha1992/gh_practice/issues/8/comments
