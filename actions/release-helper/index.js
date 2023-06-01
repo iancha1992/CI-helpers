9750e12d254b2080b23f75a04c8de2a73481d5f6
iancha1992 <heec@google.com>
2023-06-01 15:24:11 -0700

    testing

--- actions/release-helper/index.js
+++ actions/release-helper/index.js
@@ -6,6 +6,17 @@ console.log("Javascript hello world!!!!!");
 const payload = github.context.payload;
 
 console.log("This is the payload")
-console.log(payload)
+console.log(payload);
 
 
+console.log("This is the issue!!!!!")
+console.log(payload.issue.number)
+
+
+
+
+
+
+
+
+// https://api.github.com/repos/iancha1992/gh_practice/issues/8/comments
