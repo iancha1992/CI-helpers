49bc2d249a370f9bbd03b5d159afcfe471482a7f
iancha1992 <heec@google.com>
2023-06-05 15:28:16 -0700

    Testing

--- actions/release-helper/index.js
+++ actions/release-helper/index.js
@@ -30,9 +30,6 @@ const issue_number = payload.issue.number;
 async function myfunction() {
     console.log("Running myfunction")
     const commentsResponse = await octokit.request(`GET /repos/iancha1992/gh_practice/issues/${issue_number}/comments`, {
-        // owner: 'OWNER',
-        // repo: 'REPO',
-        // issue_number: '9',
         headers: {
             'X-GitHub-Api-Version': '2022-11-28'
         }
@@ -51,19 +48,21 @@ async function myfunction() {
     }
 
     // https://api.github.com/repos/bazelbuild/bazel/issues/18305
-    const git_issue = await octokit.request(`GET /repos/iancha1992/gh_practice/issues/${issue_number}`, {
-        // owner: 'OWNER',
-        // repo: 'REPO',
-        // issue_number: '9',
-        headers: {
-            'X-GitHub-Api-Version': '2022-11-28'
-        }
-    });
+    // const git_issue = await octokit.request(`GET /repos/iancha1992/gh_practice/issues/${issue_number}`, {
+    //     // owner: 'OWNER',
+    //     // repo: 'REPO',
+    //     // issue_number: '9',
+    //     headers: {
+    //         'X-GitHub-Api-Version': '2022-11-28'
+    //     }
+    // });
+    // console.log("git_issue?")
+    // console.log(git_issue.data.state)
 
-    console.log("git_issue?")
-    console.log(git_issue.data.state)
 
-    if ((git_issue.data.state == "closed") && (hasForkComment(commentsResponse.data) == true)) {
+
+
+    if ((payload.issue.state == "closed") && (hasForkComment(commentsResponse.data) == true)) {
         console.log("Good to cherrypick!")
     }
 
@@ -84,16 +83,6 @@ console.log(payload);
 
 console.log("This is the issue body")
 console.log(payload.issue.body)
-
-
-
-
-
-
-
-
-
-
 
 
 
